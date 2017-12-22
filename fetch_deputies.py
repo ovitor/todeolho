61f0bd166a5af51b755bbee671c0a4de57b8fec9
Vitor Carvalho <vitorcarvalhoml@gmail.com>
2017-12-22 12:21:10 -0300

    refactoring code

--- fetch_deputies.py
+++ fetch_deputies.py
@@ -8,22 +8,35 @@ from utils import setup_logging
 
 ALCE_URL = 'https://www.al.ce.gov.br/index.php/deputados/nomes-e-historico/'
 
-def init():
-    logger.info("init executed")
-    driver = webdriver.PhantomJS()
-    driver.get(ALCE_URL)
+def init_phantomjs_driver(*args, **kwargs):
+
+    headers = { 'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language':'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0',
+        'Connection': 'keep-alive'
+    }
+
+    for key, value in headers.items():
+        webdriver.DesiredCapabilities.PHANTOMJS['phantomjs.page.customHeaders.{}'.format(key)] = value
+
+    webdriver.DesiredCapabilities.PHANTOMJS['phantomjs.page.settings.userAgent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'
+
+    driver =  webdriver.PhantomJS(*args, **kwargs)
+    driver.set_window_size(1400,1000)
+
     return driver
 
 def run(driver):
     deputies = list()
-    div = driver.find_element_by_xpath("//div[@id='articlepxfontsize1']")
+    article = driver.find_element_by_xpath("//div[@id='articlepxfontsize1']")
     for i in range(1,100):
         try:
-            div_aux = div.find_element_by_xpath("div[%d]/div" % (i))
+            div_aux = article.find_element_by_xpath("div[%d]/div" % (i))
             link = div_aux.find_element_by_xpath("a")
             info = div_aux.text.split('\n')
             deputies.append({'nome': info[0], 'partido': info[1], 'link': link.get_attribute('href')})
         except Exception as e:
+            logger.error(e)
             break
 
     header_keys = list(deputies[0].keys())
@@ -41,8 +54,11 @@ def close(driver):
 if __name__ == '__main__':
     setup_logging()
     logger = logging.getLogger(__name__)
-    driver = init()
-    header_keys, deputies = run(driver)
+
+    browser = init_phantomjs_driver()
+    browser.get(ALCE_URL)
+
+    header_keys, deputies = run(browser)
     persist(header_keys, deputies)
 
-    close(driver)
+    close(browser)
